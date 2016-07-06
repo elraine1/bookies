93feddaa83badd8e1dc695d4ba0d0c19e1828463
Yun SeokMin <elraine@naver.com>
2016-07-06 14:30:29 +0900

    main form

---
+++ html/template/menu.php
@@ -0,0 +1,10 @@
+		
+<div id="menu">
+	<ul>
+		<li><a href="#"><h4>소개</h4></a></li>
+		<li><a href="#"><h4>출결 조회</h4></a></li>
+		<li><a href="#"><h4>성적 조회</h4></a></li>
+		<li><a href="#"><h4>내 정보 보기</h4></a></li>
+		<li><a href="#"><h4>학생 검색<br>(교사 전용)</h4></a></li>
+	</ul>
+</div>
