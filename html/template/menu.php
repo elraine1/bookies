fe96bd21185b2486db6bf7eb60ec2c56449759cd
Yun SeokMin <elraine@naver.com>
2016-07-06 14:35:57 +0900

    modify title, header

--- html/template/menu.php
+++ html/template/menu.php
@@ -1,10 +1,10 @@
 		
 <div id="menu">
 	<ul>
-		<li><a href="#"><h4>소개</h4></a></li>
-		<li><a href="#"><h4>출결 조회</h4></a></li>
-		<li><a href="#"><h4>성적 조회</h4></a></li>
-		<li><a href="#"><h4>내 정보 보기</h4></a></li>
-		<li><a href="#"><h4>학생 검색<br>(교사 전용)</h4></a></li>
+		<li><a href="#"><h4>메뉴1</h4></a></li>
+		<li><a href="#"><h4>메뉴2</h4></a></li>
+		<li><a href="#"><h4>메뉴3</h4></a></li>
+		<li><a href="#"><h4>메뉴4</h4></a></li>
+		<li><a href="#"><h4>메뉴5<br>(관리자 전용)</h4></a></li>
 	</ul>
 </div>
