ac9542e17b9eda499359d867f2f2116a7f9db578
Yun SeokMin <elraine@naver.com>
2016-07-07 16:33:53 +0900

    add admin mode(2)

--- html/template/loginbar.php
+++ html/template/loginbar.php
@@ -19,8 +19,8 @@
 		//	printf("<span><b>%s</b> 님 반갑습니다.</span>", $_SESSION['name']);			// 로그인시 이름 외 추가 정보 출력 여부 고민해볼 것.
 
 		printf("<form action='../user/user_profile.php' method='post'>");
-		printf("<span>%s 님 환영합니다. </span>", $_SESSION['name']);
-		printf("<input type='submit' value='내정보보기'>");		
+		printf("<span><b>%s</b> 님 환영합니다. </span>", $_SESSION['name']);
+		printf("<input type='submit' value='내정보보기'>");	
 		printf("</form>");
 		printf("<a href='../login/logout.php'><button>로그아웃</button></a>");
 		
