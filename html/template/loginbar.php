b5ce3663267297c31e79c4d862078e85f5580d08
khk <coinking1@naver.com>
2016-07-07 12:06:34 +0900

    로그인바 수정

--- html/template/loginbar.php
+++ html/template/loginbar.php
@@ -3,7 +3,8 @@
 	<b>ID: </b><input type="text" id="username"> 
 	<b>PW: </b><input type="password" id="password"> 
 	
-	<a href="/../login/index.php"><button>로그인</button></a>
+	<a href="/../login/protected_page.php"><button>로그인</button></a>
+	<a href="/../login/register_page.php"><button>회원가입</button></a>
 	
 	
 </div>
