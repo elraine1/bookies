c6ded64b800da3c19e9193cd504828148b080f86
khk <coinking1@naver.com>
2016-07-14 14:11:17 +0900

    footer수정

--- html/template/footer.php
+++ html/template/footer.php
@@ -1,3 +1,28 @@
+<html>
+<style type="text/css">
+		table{
+			margin-left: 25px;
+		}
+		table, tr, th, td{
+			border: 1px solid red;
+			border-collapse: collapse;
+			text-align:center;
+		}
+		th{
+			background-color: salmon;
+		}
+	
+	</style>
+
 <div id="footer">
-	<h3>Footer</h3>
-</div>
+	<h3>BOOKIES란,'book'과'cookies'의 합성어입니다!</h3>
+서울특별시 금천구 가산동<br>
+(주)북키스 Tel 02-1544-0998/Fax 02-6513-0998 <br>
+고객센터 : 1544-0998<br>
+사업자등록넘버 : 123-45-67890/통신판매업 신고번호 : 제2016-서울금천-0123호
+벤처기업인증 : 따위 없다
+
+
+	
+	</div>
+</html>
